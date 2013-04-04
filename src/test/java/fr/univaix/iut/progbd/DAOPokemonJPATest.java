66fd473508a29319ff67c5facd6e8df4a238be06
Sébastien NEDJAR <nedseb@goldaire.com>
2013-04-04 11:57:07 +0300

    Update DAOPokemonJPATest.java

--- src/test/java/fr/univaix/iut/progbd/DAOPokemonJPATest.java
+++ src/test/java/fr/univaix/iut/progbd/DAOPokemonJPATest.java
@@ -30,7 +30,7 @@ public class DAOPokemonJPATest {
     @BeforeClass
     public static void initTestFixture() throws Exception {
         // Get the entity manager for the tests.
-        entityManagerFactory = Persistence.createEntityManagerFactory("pokebattlePU");
+        entityManagerFactory = Persistence.createEntityManagerFactory("pokebattlePUTest");
         entityManager = entityManagerFactory.createEntityManager();
 
         Connection connection = ((EntityManagerImpl) (entityManager.getDelegate())).getServerSession().getAccessor().getConnection();
